7c3fb6e6e986ba9ff7fb8b07c55531cd3fd4c293
Talim Ali <talim392004@gmail.com>
2026-09-14 21:53:37 +0530

    Add files via upload

---
+++ Pyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    int n=4;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(j=1;j<=i;j++){
+            cout<<j;
+        }
+        for(j=1;j<=i-1;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
